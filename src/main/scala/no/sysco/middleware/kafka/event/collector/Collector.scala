0dc2fa104e06b0cc7dc454a9208958cecf75d56c
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-10-05 18:48:00 +0200

    fix: entry point

--- src/main/scala/no/sysco/middleware/kafka/event/collector/Collector.scala
+++ src/main/scala/no/sysco/middleware/kafka/event/collector/Collector.scala
@@ -1,28 +1,28 @@
 package no.sysco.middleware.kafka.event.collector
 
-import akka.actor.{ Actor, ActorRef, ActorSystem }
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import no.sysco.middleware.kafka.event.collector.cluster.ClusterManager
-import no.sysco.middleware.kafka.event.collector.internal.{ EventConsumer, EventProducer, EventRepository }
+import no.sysco.middleware.kafka.event.collector.internal.{EventConsumer, EventProducer, EventRepository}
 import no.sysco.middleware.kafka.event.collector.topic.TopicManager
 import no.sysco.middleware.kafka.event.proto.collector.CollectorEvent
 
 import scala.concurrent.ExecutionContext
 
-object Collector {
+object Collector extends App {
   implicit val actorSystem: ActorSystem = ActorSystem("collector")
   implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContext = actorSystem.dispatcher
 
-  new Collector()
+  def props(): Props = Props(new Collector())
+
+  actorSystem.actorOf(props())
 }
 
 /**
+  * Main application actor. Manage entity managers to collect and publish events from a Kafka Cluster.
   *
-  * @param actorSystem
-  * @param actorMaterializer
-  * @param executionContext
   */
 class Collector(implicit actorSystem: ActorSystem, actorMaterializer: ActorMaterializer, executionContext: ExecutionContext) extends Actor {
   val config: CollectorConfig = new CollectorConfig(ConfigFactory.load())
